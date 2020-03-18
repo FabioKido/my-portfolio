5fe1eedcd3b11d1fc211e744534293e8cb412929
FabioKido <fabiohenryquemesquita@gmail.com>
2020-03-18 20:05:10 -0300

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./styles.css";
+
+const Header = () => (<header id="main-header">
+		<div className="nome">
+			
+		</div>
+		<div className="links">
+			<a href="/">Home</a>
+			<a href="/register">Registrar</a>
+			<a href="/graficos">Gráfico</a>
+		</div>
+	</header>
+);
+
+export default Header;
