43687312cf8dc1568f055aa9bdf84c4746c1ba8e
FabioKido <fabiohenryquemesquita@gmail.com>
2020-03-19 00:34:56 -0300

    Updates

---
+++ precache-manifest.a179c783e3e99268ccc90f6757d9c4c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65898fbadf55c0fbf7b16287939221c6",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "8428af08e4f990a09604",
+    "url": "/my-portfolio/static/css/main.7c4ebd96.chunk.css"
+  },
+  {
+    "revision": "7e2928f59ec45c069ea6",
+    "url": "/my-portfolio/static/js/2.2f3817ec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-portfolio/static/js/2.2f3817ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8428af08e4f990a09604",
+    "url": "/my-portfolio/static/js/main.1dcc3cbf.chunk.js"
+  },
+  {
+    "revision": "d4a6f897414192edb936",
+    "url": "/my-portfolio/static/js/runtime-main.7e7d7162.js"
+  }
+]);
